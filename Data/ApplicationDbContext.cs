9ef241f54b1e7ff2c56339c581ee5f77837cce3a
Allan Debastiani <allandeba@yahoo.com.br>
2023-12-10 17:14:31 -0300

    feat DBContext for table IBGE

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using DesafioBlazor.Data.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,4 +6,23 @@ namespace DesafioBlazor.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
+    public DbSet<IBGE> IBGE { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        _ = builder.Entity<IBGE>(entity =>
+        {
+            entity.Property(x => x.Id).IsRequired().HasMaxLength(7);
+            entity.Property(x => x.State).HasMaxLength(2);
+            entity.Property(x => x.City).HasMaxLength(80);
+
+            entity.HasKey(x => x.Id);
+
+            entity.HasIndex(x => x.Id).IsUnique();
+            entity.HasIndex(x => x.State);
+            entity.HasIndex(x => x.City);
+        });
+    }
 }
---
+++ Data/Models/IBGE.cs
@@ -0,0 +1,8 @@
+namespace DesafioBlazor.Data.Models;
+
+public class IBGE
+{
+    public string Id { get; set; }
+    public string  State { get; set; }
+    public string City { get; set; }
+}
