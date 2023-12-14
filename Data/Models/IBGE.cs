d951992003c755196e6aecdc1b902e956d50ddf7
Allan Debastiani <allandeba@yahoo.com.br>
2023-12-13 21:22:19 -0300

    fix warnings

--- Data/Models/IBGE.cs
+++ Data/Models/IBGE.cs
@@ -2,7 +2,7 @@ namespace DesafioBlazor.Data.Models;
 
 public class IBGE
 {
-    public string Id { get; set; }
-    public string  State { get; set; }
-    public string City { get; set; }
+    public string Id { get; set; } = string.Empty;
+    public string  State { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
 }
