f20e164111fd856ed7e08b45bcb87cc193b88bd5
Allan Debastiani <allandeba@yahoo.com.br>
2023-12-13 23:35:59 -0300

    feat DataAnnotation for validation

--- Data/Models/IBGE.cs
+++ Data/Models/IBGE.cs
@@ -1,8 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using DesafioBlazor.Data.SharedContext;
+
 namespace DesafioBlazor.Data.Models;
 
 public class IBGE
 {
+    [Required(ErrorMessage = Const.RequiredMessage)]
+    [Display(Name = "Código")]
+    [MaxLength(7, ErrorMessage = Const.LengthMessage)]
+    [RegularExpression(Const.OnlyNumbersExpression, ErrorMessage = Const.OnlyNumbersMessage)]
     public string Id { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = Const.RequiredMessage)]
+    [Display(Name = "Estado")]
+    [MaxLength(2, ErrorMessage = Const.LengthMessage)]
+    [RegularExpression(Const.OnlyStringExpression, ErrorMessage = Const.OnlyLettersMessage)]
     public string  State { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = Const.RequiredMessage)]
+    [Display(Name = "Cidade")]
+    [MaxLength(80, ErrorMessage = Const.LengthMessage)]
+    [RegularExpression(Const.OnlyStringExpression, ErrorMessage = Const.OnlyLettersMessage)]
     public string City { get; set; } = string.Empty;
 }
